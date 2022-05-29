9649d1d616ce6c7c3519ab174a6a54bfe6aeaeb7
FlutLab.io <>
2022-05-29 02:17:35 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,80 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'dart:math';
 
-void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dados'),
-          backgroundColor: Colors.red,
-        ),
-        body: _novaPaginaDados(),
-      ),
-    ),
-  );
+Future<Album> fetchAlbum() async {
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load album');
+  }
 }
 
-class _novaPaginaDados extends StatefulWidget {
-  const _novaPaginaDados({Key? key}) : super(key: key);
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  State<_novaPaginaDados> createState() => new __novaPaginaDadosState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class __novaPaginaDadosState extends State<_novaPaginaDados> {
-  Random random = new Random();
-  int d01 = 1;
-  int d02 = 2;
+class _MyAppState extends State<MyApp> {
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  d01 = random.nextInt(6) + 1;
-                });
-              },
-              child: Image.asset('images/dado$d01.png'),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  d02 = random.nextInt(6) + 1;
-                });
-              },
-              child: Image.asset('images/dado$d02.png'),
-            ),
-          ),
-        ],
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Fetch Data Example'),
+          ),
+          body: Center(
+              child: FutureBuilder<Album>(
+                  future: futureAlbum,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Text(snapshot.data!.title);
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+
+                    return const CircularProgressIndicator();
+                  }))),
     );
   }
 }
